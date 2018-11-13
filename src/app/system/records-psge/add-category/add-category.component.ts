6a6bec7d4617378e157e0d60f9b2fcc4d6df5902
antonsos <anton1986.2012@gmail.com>
2018-11-13 16:19:34 +0700

    smashed into components

---
+++ src/app/system/records-psge/add-category/add-category.component.ts
@@ -0,0 +1,58 @@
+import {Component, EventEmitter, OnDestroy, OnInit, Output} from '@angular/core';
+import {NgForm} from '@angular/forms';
+import {CategoryService} from '../../../shared/services/category.service';
+import {CategoryModel} from '../../../shared/models/category.model';
+import {MessageModel} from '../../../shared/models/message.model';
+import {Subscription} from 'rxjs';
+
+@Component({
+  selector: 'app-add-category',
+  templateUrl: './add-category.component.html',
+  styleUrls: ['./add-category.component.css']
+})
+export class AddCategoryComponent implements OnInit, OnDestroy {
+
+  constructor(private categoryService: CategoryService) { }
+
+  sub1: Subscription;
+  message: MessageModel;
+  @Output() onCategoryAdd = new EventEmitter<CategoryModel>();
+  public isValid: boolean;
+
+  public onFormSubmit(form: NgForm) {
+    if (!form.invalid) {
+      const { name, capacity } = form.value;
+      const data = new CategoryModel(name, capacity);
+
+      this.sub1 = this.categoryService.addCategory(data)
+        .subscribe(
+          (category: CategoryModel) => {
+            this.onCategoryAdd.emit(category);
+            form.reset();
+
+            this.message = new MessageModel('success', 'Категория успешно создана');
+
+            setTimeout(() => {
+              this.message.text = '';
+            }, 3000);
+          },
+          error => {
+            this.message = new MessageModel('danger', 'Сервер не отвечает');
+
+            setTimeout(() => {
+              this.message.text = '';
+            }, 3000);
+          }
+        );
+    }
+  }
+
+  ngOnInit() {
+    this.message = new MessageModel('success', '');
+  }
+
+  ngOnDestroy() {
+    if (this.sub1) { this.sub1.unsubscribe(); }
+  }
+
+}
