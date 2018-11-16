2acefb026d3e104216836eea154532faa38d91a9
antonsos <anton1986.2012@gmail.com>
2018-11-16 19:32:01 +0700

    add 404 page and spinner

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,8 +1,10 @@
-import { NgModule } from "@angular/core";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { LoaderComponent } from './components/loader/loader.component';
 
 @NgModule({
     imports: [ ReactiveFormsModule, FormsModule ],
-    exports: [ ReactiveFormsModule, FormsModule ]
+    exports: [ ReactiveFormsModule, FormsModule, LoaderComponent ],
+    declarations: [LoaderComponent],
 })
-export class SharedModule {}
+export class SharedModule { }
